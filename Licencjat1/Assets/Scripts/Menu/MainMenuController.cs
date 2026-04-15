e6e82922f1ede9e6c6ad1a3da93f00fa06c82faa
JakubPacholak <Jakubpacholak1@gmail.com>
2026-04-15 14:04:44 +0200

    New menu windows

--- Licencjat1/Assets/Scripts/Menu/MainMenuController.cs
+++ Licencjat1/Assets/Scripts/Menu/MainMenuController.cs
@@ -1,48 +1,71 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenuController : MonoBehaviour
 {
-    [Header("Ustawienia Scen")]
-    [Tooltip("Wpisz tu nazw? sceny z Twoim intrem, np. IntroScene")]
-    public string introSceneName = "IntroScene";
+    [Header("Panele Menu")]
+    public GameObject panelMainMenu;
+    public GameObject panelLevels;
+    public GameObject panelCollection;
+    public GameObject panelCollectionDetails;
+    public GameObject panelOptions;
+    public GameObject panelKeybinds;
 
-    [Tooltip("Wpisz tu nazw? sceny z gr?, do tej pory by?o to SampleScene")]
-    public string gameSceneName = "SampleScene";
+    [Header("Przyciski Opcji (Teksty)")]
+    public TextMeshProUGUI musicButtonText;
+    public TextMeshProUGUI soundButtonText;
 
-    public void StartGame()
+    private bool isMusicOn = true;
+    private bool isSoundOn = true;
+
+    void Start()
     {
-        if (PlayerPrefs.HasKey("IntroPlayed"))
-        {
-            SceneManager.LoadScene(gameSceneName);
-        }
-        else
-        {
-            SceneManager.LoadScene(introSceneName);
-        }
+        ShowPanel(panelMainMenu);
+        UpdateOptionsUI();
+    }
+    private void ShowPanel(GameObject panelToShow)
+    {
+        panelMainMenu.SetActive(false);
+        panelLevels.SetActive(false);
+        panelCollection.SetActive(false);
+        panelCollectionDetails.SetActive(false);
+        panelOptions.SetActive(false);
+        panelKeybinds.SetActive(false);
+
+        panelToShow.SetActive(true);
     }
 
-    public void QuitGame()
+    public void OpenMainMenu() => ShowPanel(panelMainMenu);
+    public void OpenLevels() => ShowPanel(panelLevels);
+    public void OpenCollection() => ShowPanel(panelCollection);
+    public void OpenCollectionDetails() => ShowPanel(panelCollectionDetails);
+    public void OpenOptions() => ShowPanel(panelOptions);
+    public void OpenKeybinds() => ShowPanel(panelKeybinds);
+
+    public void ToggleMusic()
     {
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#else
+        isMusicOn = !isMusicOn;
+        UpdateOptionsUI();
+    }
+
+    public void ToggleSound()
+    {
+        isSoundOn = !isSoundOn;
+        UpdateOptionsUI();
+    }
+
+    private void UpdateOptionsUI()
+    {
+        if (musicButtonText != null)
+            musicButtonText.text = isMusicOn ? "Music: ON" : "Music: OFF";
+
+        if (soundButtonText != null)
+            soundButtonText.text = isSoundOn ? "Sound: ON" : "Sound: OFF";
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Wychodzenie z gry...");
         Application.Quit();
-#endif
-    }
-
-    public void OpenLevels()
-    {
-        Debug.Log("Otwieram panel poziomów...");
-    }
-
-    public void OpenCollections()
-    {
-        Debug.Log("Otwieram panel kolekcji...");
-    }
-
-    public void OpenOptions()
-    {
-        Debug.Log("Otwieram panel opcji...");
     }
 }
